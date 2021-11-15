3382e1778daa6ae3c046f587466ece7eddf0870b
timilyo80 <78914833+timilyo80@users.noreply.github.com>
2021-11-14 19:18:19 -0500

    lol non c'est fini pour moi j'ai autre chose a faire

---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    static class AppConfiguration
+    {
+        public static IConfiguration configuration;
+
+        public static string GetValue(string s)
+        {
+            return "fuckthis";
+        }
+
+        private static void initConfig()
+        {
+
+        }
+    }
+}
